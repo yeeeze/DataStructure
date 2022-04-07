1d663b5ca917fd4f89b848fe56a366f5aa1fbf84
yeezze <86194303+yeezze@users.noreply.github.com>
2022-04-07 19:28:12 +0900

    Refactor: stack 괄호 짝 맞추기 강의 코드

--- src/DS/Stack/ListStack/Main.java
+++ src/DS/Stack/ListStack/Main.java
@@ -4,12 +4,64 @@ package DS.Stack.ListStack;
 
 public class Main {
     public static void main(String[] args) {
-        ListStack<String> stack = new ListStack<>();
-        String[] s1 = {"{", "{", "(", ")", "{", "(", ")", "}", "}", "}"};
-        String[] s2 = {"{", "{", "(", ")", "{", "(", ")", "}", ")", "(", ")", "}"};
+        String str = new String("{{(){()}}}");
+        System.out.println(str);
+        checkPaired(str);
 
-        bracket(stack, s1);
-        bracket(stack, s2);
+        str = new String("{{(){()})()}");
+        System.out.println("\n" + str);
+        checkPaired(str);
+
+        str = new String("{{()(}}}");
+        System.out.println("\n" + str);
+        checkPaired(str);
+
+        str = new String("{{(())}");
+        System.out.println("\n" + str);
+        checkPaired(str);
+
+        str = new String(")}");
+        System.out.println("\n" + str);
+        checkPaired(str);
+    }
+
+    private static void checkPaired(String str) {
+        ListStack<Character> stack = new ListStack<>();
+        int mismatch = -1;
+
+        for(int i = 0; i < str.length() && mismatch == -1; i++) {
+            switch (str.charAt(i)) {
+                case '(':
+                case '{': stack.push(str.charAt(i)); break;
+                case ')':
+                    if(stack.isEmpty()) {
+                        mismatch = i;
+                    }
+                    else if(stack.pop() != '(') {
+                        mismatch = i;
+                    }
+                    break;
+
+                case '}':
+                    if(stack.isEmpty()) {
+                        mismatch = i;
+                    }
+                    else if(stack.pop() != '{') {
+                        mismatch = i;
+                    }
+                    break;
+            }
+        }
+
+        if(mismatch != -1) {
+            System.out.println(str.substring(0, mismatch) + "^^^부터 괄호 쌍이 맞지 않습니다.");
+        }
+        else if(!stack.isEmpty()) {
+            System.out.println(str + "^^^부터 괄호 쌍이 맞지 않습니다.");
+        }
+        else {
+            System.out.println("괄호 쌍이 맞습니다.");
+        }
     }
 
     public static void bracket(ListStack<String> stack, String[] s) {
